f55abc8dfd18c02eafa26db590a882f655c24256
leehwui <leehwui@163.com>
2018-07-10 17:25:56 +0800

    Remove # from url

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Calculator from '@/components/Calculator'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
-    //{
-    //  path: '/',
-    //  name: 'HelloWorld',
-    //  component: HelloWorld
-    //}
     {
       path: '/',
       name: 'Calculator',
